7253c870c587d7f86cb657cbb0da6ec6b4a7eb81
tulayevibrohim <tulayevibrohimjon@gmail.com>
2023-02-07 20:55:45 +0500

    hi

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        int i = 1;
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,20 @@
 package lang.print.gaps.task5;
 
+
+
+
 public class TimeConvertor {
     public void convert(float minutes) {
+     minutes = 12f;
+     float seconds = minutes*60;
+     System.out.println(seconds);
+
     }
+
+    public static void main(String[] args) {
+
+    }
+
 }
+
+
