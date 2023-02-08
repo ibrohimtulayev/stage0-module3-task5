e468823144efb08eac5420b1db0caa6bbcf33cf0
tulayevibrohim <tulayevibrohimjon@gmail.com>
2023-02-08 11:43:27 +0500

    hi

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -5,14 +5,10 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public static void convert(float minutes) {
-     minutes = 12;
+     minutes = 12f;
      float seconds = minutes*60;
      System.out.println(seconds);
 
-    }
-
-    public static void main(String[] args) {
-       convert(12);
     }
 
 }
