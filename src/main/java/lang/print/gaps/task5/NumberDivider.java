02aee9a40f244327aaf980171c54a4211826b41c
tulayevibrohim <tulayevibrohimjon@gmail.com>
2023-02-08 12:05:36 +0500

    hi

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        toBeDivided = 15;
         int result = toBeDivided / 5;
         System.out.println(result);
     }
